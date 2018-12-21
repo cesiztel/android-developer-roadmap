3bd262dca26e59cee36ec5d7e1efefbccffe0bcd
Cesar Izquierdo Tello <cesar@codeandcoding.com>
2018-12-21 13:11:43 +0100

    feat: finish Chapter 1 adding listeners to the buttons

--- the-big-nerd-ranch-guide-book/GeoQuiz/app/src/main/java/com/cesiztel/geoquiz/QuizActivity.java
+++ the-big-nerd-ranch-guide-book/GeoQuiz/app/src/main/java/com/cesiztel/geoquiz/QuizActivity.java
@@ -2,12 +2,40 @@ package com.cesiztel.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT)
+                .show();
+            }
+        });
+
+        mFalseButton = findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT)
+                .show();
+            }
+        });
     }
 }
