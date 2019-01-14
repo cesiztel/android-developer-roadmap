7a64586f258f554c764cc6fd4186569c58065713
Cesar Izquierdo Tello <cesar@codeandcoding.com>
2019-01-14 15:28:11 +0100

    feat: add animation to show how works the build version code check

--- the-big-nerd-ranch-guide-book/GeoQuiz/app/src/main/java/com/cesiztel/geoquiz/CheatActivity.java
+++ the-big-nerd-ranch-guide-book/GeoQuiz/app/src/main/java/com/cesiztel/geoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package com.cesiztel.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -46,6 +50,24 @@ public class CheatActivity extends AppCompatActivity {
                 mAnswerTextView.setText((mAnswerIsTrue) ?
                         R.string.true_button: R.string.false_button);
                 setAnswerShownResult(true);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
         });
     }
