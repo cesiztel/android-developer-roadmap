1cdb7f224418b4813d832b0264a05f34c896b23a
Cesar Izquierdo Tello <cesar@codeandcoding.com>
2019-01-14 15:55:46 +0100

    feat: start project CriminalIntent

---
+++ the-big-nerd-ranch-guide-book/CriminalIntent/app/src/main/java/com/cesiztel/criminalintent/CrimeActivity.java
@@ -0,0 +1,13 @@
+package com.cesiztel.criminalintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
