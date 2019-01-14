193cfa9ed7d29ad3e62b7d0137c4db2fdad460f1
Cesar Izquierdo Tello <cesar@codeandcoding.com>
2019-01-14 10:41:44 +0100

    feat: explore activity lifecycle using logs and overriding all the activity lifecycle methods

--- the-big-nerd-ranch-guide-book/GeoQuiz/app/src/main/java/com/cesiztel/geoquiz/QuizActivity.java
+++ the-big-nerd-ranch-guide-book/GeoQuiz/app/src/main/java/com/cesiztel/geoquiz/QuizActivity.java
@@ -2,12 +2,14 @@ package com.cesiztel.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    public static String TAG = "QuizActivity";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -27,6 +29,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = findViewById(R.id.question_text_view);
@@ -56,6 +59,36 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     private void updateQuestion() {
